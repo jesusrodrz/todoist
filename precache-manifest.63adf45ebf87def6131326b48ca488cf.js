e5e4e96f8419b404611acd2b12aa84564d07df54
Jesus Rodrguez <jesus.rodrz97@gmail.com>
2020-02-19 00:57:53 -0400

    Updates

---
+++ precache-manifest.63adf45ebf87def6131326b48ca488cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945a9a966e72323097cd44f4735a9d13",
+    "url": "/todoist/index.html"
+  },
+  {
+    "revision": "196b3f790368247961f7",
+    "url": "/todoist/static/css/main.8d8eb27b.chunk.css"
+  },
+  {
+    "revision": "f651cc50b966e072578c",
+    "url": "/todoist/static/js/2.0dfc27d3.chunk.js"
+  },
+  {
+    "revision": "196b3f790368247961f7",
+    "url": "/todoist/static/js/main.9c654986.chunk.js"
+  },
+  {
+    "revision": "3caa07b678dd9d3feaf4",
+    "url": "/todoist/static/js/runtime-main.1352a7a7.js"
+  }
+]);
